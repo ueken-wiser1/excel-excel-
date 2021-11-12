19ee815e663102d974d35ee706ac93b320175234
ueken-wiser1 <cola.with.plum.brandy@gmail.com>
2021-11-12 17:41:21 +0900

    main

---
+++ 20211110_市場データ前処理.bat
@@ -0,0 +1,6 @@
+python C:/Users/touko/program\github/excel-excel-/excel-excel-/preprocess-marketdata/20211108_regulation.py
+python C:/Users/touko/program\github/excel-excel-/excel-excel-/preprocess-marketdata/20211109_karauri.py
+python C:/Users/touko/program\github/excel-excel-/excel-excel-/preprocess-marketdata/20211109_mashitan.py
+python C:/Users/touko/program\github/excel-excel-/excel-excel-/preprocess-marketdata/20211109_yushikashikabu.py
+
+pause
